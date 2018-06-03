c5a68ba19c02c3e976529c0abc3fcd9f6b288986
Mohammad Abu Naeem <33114292+naeeem96@users.noreply.github.com>
2018-06-03 17:31:14 +0600

    Add files via upload

---
+++ airTicket/src/Calculate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NAeeM
+ */
+public class Calculate {
+    double cost;
+    double calculationL(double count) 
+    {
+        return count * 2000;
+    }
+    double calculationB(double count) 
+    {
+        return count * 5000;
+    }
+    
+}
